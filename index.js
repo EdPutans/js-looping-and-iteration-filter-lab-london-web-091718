a9e05cdd91eab768cb88e0df61b58cd19662ffd8
Ed Putans <edputans@gmail.com>
2018-10-28 22:48:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file
+function findMatching(array, query){
+  let result=[]
+  for (thing of array){
+    if (thing.toLowerCase() == query.toLowerCase()){
+      result.push(thing)
+    }
+  }
+  return result
+}
+
+function fuzzyMatch(array, query){
+  let result=[]
+  for (thing of array){
+    if (thing.startsWith(query)){
+      result.push(thing)
+    }
+  }
+  return result
+}
+
+function matchName(object, query){
+  let result =[]
+  for (person of object){
+    let nameruni = person['name']
+    if ( nameruni.toLowerCase() === query.toLowerCase()){
+      result.push(person)
+    }
+  }
+  return result
+}
